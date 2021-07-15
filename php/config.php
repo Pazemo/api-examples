21b9b9fb90efc09b06f942c6b8cb2cc88e8c4dee
Maksum Rifai <maksum.rifai@gmail.com>
2021-07-15 10:47:29 +0700

    Create config.php
    Define variables to use in API Request

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('AUTH_URL', 'http://api.stg.pazemo.com/users/me');
+define('PING_URL', 'http://api.stg.pazemo.com/ping');
+define('ACCOUNT_URL', 'http://api.stg.pazemo.com/users/{userId}/accounts');
+define('TOKEN', 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx');
+
+?>
